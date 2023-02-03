09b1f1ddd0df6c7b9f274e92e3e85942d2788146
cguevara6 <cguevara6@dxc.com>
2023-02-03 18:08:38 +0800

    fix: [day7Assignment] handle exceptions

--- day7Assignment/moduleMongo.js
+++ day7Assignment/moduleMongo.js
@@ -1,6 +1,13 @@
 const { MongoClient } = require("mongodb")
 
 const uri = "mongodb://127.0.0.1:27017/"
+const insertDoc = [
+    {empId:201,empName:"Julie",salary:3456,deptId:"E1"},
+    {empId:202,empName:"Ryan",salary:2366,deptId:"E1"},
+    {empId:203,empName:"Kylie",salary:12456,deptId:"E2", "_id": 777},
+    {empId:204,empName:"Cleo",salary:4569,deptId:"E3"},
+    {empId:205,empName:"Via",salary:37800,deptId:"E2"},
+]
 
 const mongoClient = new MongoClient(uri, { useUnifiedTopology: true })
 
@@ -9,18 +16,16 @@ async function run() {
         var client = await mongoClient.connect()
         var dbName = client.db("dxcDB")
         var colName = dbName.collection("emp")
-        const insertDoc = [
-            {empId:201,empName:"Julie",salary:3456,deptId:"E1"},
-            {empId:202,empName:"Ryan",salary:2366,deptId:"E1"},
-            {empId:203,empName:"Kylie",salary:12456,deptId:"E2"},
-            {empId:204,empName:"Cleo",salary:4569,deptId:"E3"},
-            {empId:205,empName:"Via",salary:37800,deptId:"E2"},
-        ]
 
-        var res = await colName.insertMany(insertDoc)
-
-        console.log("Inserted Ids: ", res.insertedIds)
-        console.log("Inserted Count: ", res.insertedCount)
+        colName.insertMany(insertDoc)
+            .then((res) => {
+                console.log("Inserted Ids: ", res.insertedIds)
+                console.log("Inserted Count: ", res.insertedCount)
+            })
+            .catch((err) => {
+                console.log("Number or emp inserted: ", err.result.insertedCount)
+                console.log("Error on insertMany: ", err.writeErrors[0])
+            })
     }
     catch (err) {
         console.log("Error: ", err)
